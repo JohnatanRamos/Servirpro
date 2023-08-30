f1cf04f50994e0761dd431f8c22ac38c097a2d0b
Jramos <johnatan.r1000@gmail.com>
2023-08-30 12:40:15 -0500

    Se agrega el section del video

---
+++ src/app/components/office-video/office-video.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-office-video',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './office-video.component.html',
+  styleUrls: ['./office-video.component.scss']
+})
+export class OfficeVideoComponent {
+
+}
