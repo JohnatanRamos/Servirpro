505972f7a474dba847ede64a4fd6100e3e3b5a06
Jramos <johnatan.r1000@gmail.com>
2023-08-30 22:47:24 -0500

    Se agrega el boton de wpp.

--- src/app/components/front-page/front-page.component.ts
+++ src/app/components/front-page/front-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GenericButtonComponent } from 'src/app/shared/generic-button/generic-button.component';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 
 @Component({
   selector: 'app-front-page',
   standalone: true,
-  imports: [CommonModule, GenericButtonComponent],
+  imports: [CommonModule, GenericButtonComponent, OverlayPanelModule],
   templateUrl: './front-page.component.html',
   styleUrls: ['./front-page.component.scss']
 })
