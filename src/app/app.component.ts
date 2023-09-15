8c63afd1922402a95ae55755ee667aa9060c3be6
Jramos <johnatan.r1000@gmail.com>
2023-09-15 14:46:31 -0500

    Se agrega el estilo sonar del boton.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'servirpro';
+  hideTooltip = true;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.hideTooltip = false;
+    }, 10000);
+  }
 
   openUrl() {
     window.open('https://wa.me/3193029089', '_blank');
